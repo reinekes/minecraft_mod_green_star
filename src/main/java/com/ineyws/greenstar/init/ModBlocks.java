dcbcf226321ac8731bc25b32bad94c62a0062280
Serge_pnz <Serge_pnz>
2025-10-08 17:32:19 +0400

    v1.3.0: Added super bright green torch - Light level 15 (maximum brightness!) - Can be placed on walls and floor - Crafting: 1 green ingot + 1 stick = 4 torches - Brighter than regular torches and glowstone

--- src/main/java/com/ineyws/greenstar/init/ModBlocks.java
+++ src/main/java/com/ineyws/greenstar/init/ModBlocks.java
@@ -2,9 +2,9 @@ package com.ineyws.greenstar.init;
 
 import com.ineyws.greenstar.IneyWSGreenStar;
 import com.ineyws.greenstar.blocks.GreenOreBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
@@ -25,6 +25,23 @@ public class ModBlocks {
                     .requiresCorrectToolForDrops()
                     .lightLevel(state -> 14)  // уровень света как у факела
                     .sound(SoundType.STONE)));
+    
+    // Супер-яркий зеленый факел (уровень света 15 - максимум!)
+    public static final RegistryObject<Block> GREEN_TORCH = BLOCKS.register("green_torch",
+            () -> new TorchBlock(Block.Properties.of(Material.DECORATION)
+                    .noCollission()
+                    .instabreak()
+                    .lightLevel(state -> 15)  // максимальный уровень света!
+                    .sound(SoundType.WOOD), ParticleTypes.FLAME));
+    
+    // Настенный зеленый факел
+    public static final RegistryObject<Block> GREEN_WALL_TORCH = BLOCKS.register("green_wall_torch",
+            () -> new WallTorchBlock(Block.Properties.of(Material.DECORATION)
+                    .noCollission()
+                    .instabreak()
+                    .lightLevel(state -> 15)
+                    .sound(SoundType.WOOD)
+                    .lootFrom(GREEN_TORCH), ParticleTypes.FLAME));
     
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
--- src/main/java/com/ineyws/greenstar/init/ModItems.java
+++ src/main/java/com/ineyws/greenstar/init/ModItems.java
@@ -22,6 +22,11 @@ public class ModItems {
             () -> new BlockItem(ModBlocks.GREEN_ORE.get(), 
                     new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
     
+    // Супер-яркий зеленый факел
+    public static final RegistryObject<WallOrFloorItem> GREEN_TORCH_ITEM = ITEMS.register("green_torch",
+            () -> new WallOrFloorItem(ModBlocks.GREEN_TORCH.get(), ModBlocks.GREEN_WALL_TORCH.get(),
+                    new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
+    
     // Супер-инструменты из зеленых слитков (в 10 раз сильнее алмазных!)
     public static final RegistryObject<SwordItem> GREEN_SWORD = ITEMS.register("green_sword",
             () -> new SwordItem(GreenItemTier.GREEN, 3, -2.4F,
